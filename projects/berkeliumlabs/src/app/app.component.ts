90a359336e6f9485c569c1898e22ce4849252c57
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-02 07:19:41 +0530

    Implement Layout service to handle light mode and dark mode

--- projects/berkeliumlabs/src/app/app.component.ts
+++ projects/berkeliumlabs/src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { LayoutService } from './layout/layout.service';
 
 @Component({
   selector: 'berkeliumlabs-root',
   imports: [RouterOutlet, NavbarComponent],
-  template: `
-  <berkeliumlabs-navbar />
-  <main class="flex-grow">
-    <router-outlet />
-  </main>`,
+  template: ` <berkeliumlabs-navbar />
+    <main class="flex-grow">
+      <router-outlet />
+    </main>`,
   styles: [],
+  providers: [LayoutService],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private _layoutService = inject(LayoutService);
+
+  ngOnInit(): void {
+    this._layoutService.setSystemTheme();
+  }
+}

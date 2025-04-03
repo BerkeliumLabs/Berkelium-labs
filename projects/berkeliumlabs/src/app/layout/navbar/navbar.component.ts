fd0290129b58fe8b85a539c224933561ac201cde
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-03 08:18:33 +0530

    Add navbar structure and styles

--- projects/berkeliumlabs/src/app/layout/navbar/navbar.component.ts
+++ projects/berkeliumlabs/src/app/layout/navbar/navbar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LayoutService } from '../layout.service';
 
 @Component({
   selector: 'berkeliumlabs-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  private _layoutService = inject(LayoutService);
 
+  themeMode: 'light' | 'dark' = 'light';
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  toggleTheme() {
+    this._layoutService.toggleTheme();
+    this.themeMode = this._layoutService.themeMode();
+  }
 }

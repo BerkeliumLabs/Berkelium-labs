6d0000a83cbc32bdb0e6b79d96f1f11c6a712d3e
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-05 00:25:28 +0530

    Update Settings component UI

--- projects/berkeliumlabs/src/app/settings/settings.component.ts
+++ projects/berkeliumlabs/src/app/settings/settings.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'berkeliumlabs-settings',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })

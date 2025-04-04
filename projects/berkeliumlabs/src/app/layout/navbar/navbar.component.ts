ae6d14622b92c8f20c009f3b14cfe38c3f760a07
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-04 06:22:46 +0530

    Update navbar animations

--- projects/berkeliumlabs/src/app/layout/navbar/navbar.component.ts
+++ projects/berkeliumlabs/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { LayoutService } from '../layout.service';
 import { Router } from '@angular/router';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'berkeliumlabs-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('toggle', [
+      state('true', style({ opacity: 1, width: 'auto' })),
+      state('false', style({ opacity: 0, width: 0 })),
+      transition('false <=> true', animate('1000ms ease-in-out'))
+    ]),
+  ],
 })
 export class NavbarComponent {
   private _layoutService = inject(LayoutService);

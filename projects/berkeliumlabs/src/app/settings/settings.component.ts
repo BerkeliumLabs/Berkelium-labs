b2423df890a4d2b4e475048ad86007fb044e4fee
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-06 09:36:24 +0530

    Implement settings update from UI

--- projects/berkeliumlabs/src/app/settings/settings.component.ts
+++ projects/berkeliumlabs/src/app/settings/settings.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'berkeliumlabs-settings',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+  private settingsFormUnsubscribe = new Subject<void>();
+  private initialFormValue!: BkAppSettings;
+  settingsForm!: FormGroup;
+  formValueChanged = false;
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm() {
+    window.berkelium
+      .readAppSettings()
+      .then((settings) => {
+        this.settingsForm = new FormGroup({
+          cacheDir: new FormControl(settings?.cacheDir),
+        });
+
+        this.initialFormValue = this.settingsForm.value;
+
+        this.settingsForm.valueChanges
+          .pipe(takeUntil(this.settingsFormUnsubscribe))
+          .subscribe((currentValue) => {
+            this.formValueChanged =
+              JSON.stringify(currentValue) !==
+              JSON.stringify(this.initialFormValue);
+          });
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
+  }
+
+  choseCacheDir() {
+    window.berkelium.setCacheDir().then((data) => {
+      if (!data?.canceled) {
+        this.settingsForm.setValue({
+          cacheDir: data?.filePaths[0],
+        });
+      }
+    });
+  }
+
+  saveForm() {
+    window.berkelium
+      .writeAppSettings(this.settingsForm.value)
+      .then(() => console.log('success'))
+      .catch((reason) => console.error(reason));
+  }
 }

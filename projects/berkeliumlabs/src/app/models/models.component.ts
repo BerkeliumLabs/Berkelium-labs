c4434fc8c4ae9f480a0309e5576b884a78aae2ad
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-06 15:03:38 +0530

    Display model list

--- projects/berkeliumlabs/src/app/models/models.component.ts
+++ projects/berkeliumlabs/src/app/models/models.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ModelItemComponent } from './model-item/model-item.component';
+import { ModelManagerService } from './model-manager.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'berkeliumlabs-models',
-  imports: [],
+  imports: [RouterLink, ModelItemComponent],
   templateUrl: './models.component.html',
-  styleUrl: './models.component.scss'
+  styleUrl: './models.component.scss',
+  providers: [ModelManagerService],
 })
-export class ModelsComponent {
+export class ModelsComponent implements OnInit, OnDestroy {
+  private _modelManager = inject(ModelManagerService);
+  private $modelListSubscription!: Subscription;
 
+  modelList!: BkHuggingfaceModelData[];
+
+  ngOnInit(): void {
+    this.initModelIncubator();
+  }
+
+  initModelIncubator(): void {
+    this.$modelListSubscription = this._modelManager
+      .fetchModelData()
+      .subscribe({
+        next: (data) => {
+          this.modelList = data as BkHuggingfaceModelData[];
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.$modelListSubscription) {
+      this.$modelListSubscription.unsubscribe();
+    }
+  }
 }

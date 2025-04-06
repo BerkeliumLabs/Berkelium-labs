d47f7f8a0789d23e945c7037e1d8e0f3e7317f7a
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-06 12:44:00 +0530

    Add HTTP client utility to fetch model data

---
+++ types/BkHuggingfaceModelData.d.ts
@@ -0,0 +1,13 @@
+interface BkHuggingfaceModelData {
+  _id?: string;
+  id?: string;
+  likes?: number;
+  trendingScore?: number;
+  private?: boolean;
+  downloads?: number;
+  tags?: string[];
+  pipeline_tag?: string;
+  library_name?: string;
+  createdAt?: string;
+  modelId?: string;
+}

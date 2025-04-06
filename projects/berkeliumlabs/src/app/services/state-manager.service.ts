6f697c823e05dd0a03ecf2d13fd5897ebaf56a1e
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-06 16:59:53 +0530

    Add State manager service

---
+++ projects/berkeliumlabs/src/app/services/state-manager.service.ts
@@ -0,0 +1,8 @@
+import { Injectable, Signal, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateManagerService {
+  selectedModel = signal<BkHuggingfaceModelData>({});
+}

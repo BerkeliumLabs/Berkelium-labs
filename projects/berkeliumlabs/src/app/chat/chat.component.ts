cf7ec416c594781ff82cd4f2ac37866607f01b06
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-09 07:07:54 +0530

    Implement and test Dropdown component for model selection in Chat UI

--- projects/berkeliumlabs/src/app/chat/chat.component.ts
+++ projects/berkeliumlabs/src/app/chat/chat.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DropdownComponent } from "../components/dropdown/dropdown.component";
 
 @Component({
   selector: 'berkeliumlabs-chat',
-  imports: [],
+  imports: [DropdownComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  settings!: BkAppSettings;
+  availableModels: BkDropdownOptions[] = [];
 
+  ngOnInit(): void {
+    this.initChat()
+  }
+
+  initChat(): void {
+    window.berkelium
+      .readAppSettings()
+      .then((settings) => {
+        if (settings) {
+          this.settings = settings;
+          if (settings.models) {
+            settings.models.forEach((model) => {
+              const modelOption: BkDropdownOptions = {
+                id: model,
+                label: model
+              };
+
+              this.availableModels.push(modelOption);
+            });
+          }
+        }
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
+  }
 }

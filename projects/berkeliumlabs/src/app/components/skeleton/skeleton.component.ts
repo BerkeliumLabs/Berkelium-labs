67b51bcd891ebfabbee9050ca5e1f28fafb8fc1e
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-10 21:33:29 +0530

    Add Skeleton component

---
+++ projects/berkeliumlabs/src/app/components/skeleton/skeleton.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'berkeliumlabs-skeleton',
+    imports: [],
+    template: `<div class="space-y-4">
+      <div class="animate-pulse bg-gray-300 rounded-full h-8 w-24 mb-2"></div>
+      <div class="animate-pulse bg-gray-300 rounded-md h-4 w-full mb-1"></div>
+      <div class="animate-pulse bg-gray-300 rounded-md h-4 w-4/5 mb-1"></div>
+      <div class="animate-pulse bg-gray-300 rounded-md h-4 w-1/2"></div>
+  </div>`
+})
+export class SkeletonComponent {}

7f50566d940754e2adede8ab4a550bdc94b34c0e
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-10 07:28:41 +0530

    Add message box component

--- projects/berkeliumlabs/src/app/chat/chat.component.ts
+++ projects/berkeliumlabs/src/app/chat/chat.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DropdownComponent } from "../components/dropdown/dropdown.component";
 import { ChatBubbleComponent } from "./chat-bubble/chat-bubble.component";
+import { MessageBoxComponent } from "./message-box/message-box.component";
 
 @Component({
   selector: 'berkeliumlabs-chat',
-  imports: [DropdownComponent, ChatBubbleComponent],
+  imports: [DropdownComponent, ChatBubbleComponent, MessageBoxComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
---
+++ projects/berkeliumlabs/src/app/chat/message-box/message-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'berkeliumlabs-message-box',
+  imports: [],
+  templateUrl: './message-box.component.html',
+  styleUrl: './message-box.component.scss'
+})
+export class MessageBoxComponent {
+
+}

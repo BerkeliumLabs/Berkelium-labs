cd8d6f9603d8a2f75fac2b5d9cad2385efbb4fe3
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-10 08:07:31 +0530

    Implement Prompt Settings form functionality

--- projects/berkeliumlabs/src/app/chat/prompt-settings/prompt-settings.component.ts
+++ projects/berkeliumlabs/src/app/chat/prompt-settings/prompt-settings.component.ts
@@ -1,12 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { DropdownComponent } from '../../components/dropdown/dropdown.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'berkeliumlabs-prompt-settings',
-  imports: [DropdownComponent],
+  imports: [DropdownComponent, ReactiveFormsModule],
   templateUrl: './prompt-settings.component.html',
   styleUrl: './prompt-settings.component.scss',
 })
-export class PromptSettingsComponent {
+export class PromptSettingsComponent implements OnInit {
   @Input() availableModels: BkDropdownOptions[] = [];
+  @Output() settingsChanged = new EventEmitter<any>();
+  
+  private formDestroyRef = inject(DestroyRef);
+  private formUnsubscribe = new Subject<void>();
+
+  promptSettingsForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.initPromptSettings();
+  }
+
+  initPromptSettings() {
+    this.promptSettingsForm = new FormGroup({
+      model: new FormControl('', [Validators.required])
+    });
+
+    this.promptSettingsForm.valueChanges
+    .pipe(takeUntil(this.formUnsubscribe))
+    .subscribe(value => {
+      console.log('Form value changed:', value);
+      this.settingsChanged.emit(value);
+    });
+
+    this.formDestroyRef.onDestroy(() => {
+      console.log('Component destroyed - cleaning up resources.');
+      this.formUnsubscribe.next();
+      this.formUnsubscribe.complete();
+      // Any other cleanup logic here
+    });
+  }
 }

9dd1f0d9246c503278f59b526b17d46d7fb3bd5f
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-19 13:52:29 +0530

    Add Settings component

---
+++ projects/berkelium-labs/src/app/settings/settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'berkeliumlabs-settings',
+  imports: [RouterLink],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.scss'
+})
+export class SettingsComponent {
+
+}

cc1fbb96c3cccd2bdb4ddd218d3dac2990d23e99
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-23 23:20:42 +0530

    Implement Summarization tool UI

--- projects/berkelium-labs/src/app/tools/summarization/summarization.component.ts
+++ projects/berkelium-labs/src/app/tools/summarization/summarization.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DropdownComponent } from '../../components/dropdown/dropdown.component';
 
 @Component({
   selector: 'berkeliumlabs-summarization',
-  imports: [RouterLink],
+  imports: [RouterLink, DropdownComponent],
   templateUrl: './summarization.component.html',
   styleUrl: './summarization.component.scss'
 })
 export class SummarizationComponent {
+  content = `Type or paste your text here...`;
+  summarizedContent = '';
+  availableModels: BkDropdownOptions[] = [];
 
+  onContentChange(event: Event) {
+    const target = event.target as HTMLDivElement;
+    this.content = target.innerHTML;
+    console.log('Content changed:', this.content);
+  }
+
+  summarize() {
+    // Implement the summarization logic here
+    this.summarizedContent = this.content; // Placeholder for the actual summarization logic
+    console.log('Summarizing content:', this.content);
+    // For example, you could call an API to get the summary
+  }
 }

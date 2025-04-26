a5f33b2f135b651d646b8612ae3ffdc6e8c6a4cd
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-26 22:22:00 +0530

    Text to text generator tool

--- projects/berkelium-labs/src/app/tools/text2text-generation/text2text-generation.component.ts
+++ projects/berkelium-labs/src/app/tools/text2text-generation/text2text-generation.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DropdownComponent } from "../../components/dropdown/dropdown.component";
+import { IndexedDBService } from '../../services/indexed-db.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'berkeliumlabs-text2text-generation',
-  imports: [RouterLink],
+  imports: [RouterLink, DropdownComponent],
   templateUrl: './text2text-generation.component.html',
   styleUrl: './text2text-generation.component.scss'
 })
 export class Text2textGenerationComponent {
+  private _dbService = inject(IndexedDBService);
 
+  toolForm!: FormGroup;
+  content = ``;
+  summarizedContent = '';
+  availableModels: BkDropdownOptions[] = [];
+  isInitializing = true;
+  inProgress = false;
 }

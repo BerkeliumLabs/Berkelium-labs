a233d2b0a83ac8699fb8ace887ee596696d38cb1
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-04-27 17:26:17 +0530

    Add validators to Summarization tool

--- projects/berkelium-labs/src/app/tools/summarization/summarization.component.ts
+++ projects/berkelium-labs/src/app/tools/summarization/summarization.component.ts
@@ -1,42 +1,49 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, model, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DropdownComponent } from '../../components/dropdown/dropdown.component';
 import { IndexedDBService } from '../../services/indexed-db.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'berkeliumlabs-summarization',
-  imports: [RouterLink, DropdownComponent],
+  imports: [RouterLink, DropdownComponent, ReactiveFormsModule],
   templateUrl: './summarization.component.html',
   styleUrl: './summarization.component.scss',
 })
 export class SummarizationComponent implements OnInit {
   private _dbService = inject(IndexedDBService);
+
+  toolForm!: FormGroup;
   content = ``;
   summarizedContent = '';
   availableModels: BkDropdownOptions[] = [];
   isInitializing = true;
-  selectedModel!: string;
+  inProgress = false;
 
   ngOnInit(): void {
-    this._dbService
-      .getAll<string>('models-summarization')
-      .subscribe((models) => {
-        if (models) {
-          models.forEach((model) => {
-            const modelOption: BkDropdownOptions = {
-              id: model,
-              label: model,
-            };
-
-            this.availableModels.push(modelOption);
-          });
-        }
-        this.isInitializing = false;
-      });
+    this.initTool();
   }
 
-  onModelChange(model: BkDropdownOptions) {
-    this.selectedModel = model.label;
+  private initTool(): void {
+    this.toolForm = new FormGroup({
+      model: new FormControl('', Validators.required),
+    });
+
+    this._dbService
+    .getAll<string>('models-summarization')
+    .subscribe((models) => {
+      if (models) {
+        models.forEach((model) => {
+          const modelOption: BkDropdownOptions = {
+            id: model,
+            label: model,
+          };
+
+          this.availableModels.push(modelOption);
+        });
+      }
+      this.isInitializing = false;
+    });
   }
 
   onContentChange(event: Event) {
@@ -53,12 +60,14 @@ export class SummarizationComponent implements OnInit {
       worker.onmessage = ({ data }) => {
         console.log('Response: ', data);
         this.summarizedContent = data[0].summary_text;
+        this.inProgress = false;
       };
 
       const data = {
         content: this.content,
-        model: this.selectedModel,
+        model: this.toolForm.get('model')?.value,
       };
+      this.inProgress = true;
       worker.postMessage(data);
     } else {
       //

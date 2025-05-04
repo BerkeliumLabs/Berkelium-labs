5dd96d84bf72395981d043423f3668616f97b751
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-05-04 10:44:57 +0530

    Remove tools

--- projects/berkelium-labs/src/app/app.routes.ts
+++ projects/berkelium-labs/src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { ChatComponent } from './chat/chat.component';
 import { ModelsComponent } from './models/models.component';
 import { ModelCardComponent } from './models/model-card/model-card.component';
 import { SettingsComponent } from './settings/settings.component';
-import { ToolsComponent } from './tools/tools.component';
-import { SummarizationComponent } from './tools/summarization/summarization.component';
-import { Text2textGenerationComponent } from './tools/text2text-generation/text2text-generation.component';
-import { TranslationComponent } from './tools/translation/translation.component';
-import { ImageToImageComponent } from './tools/image-to-image/image-to-image.component';
 
 export const routes: Routes = [
   {
@@ -29,26 +24,6 @@ export const routes: Routes = [
   {
     path: 'models/:modelId',
     component: ModelCardComponent,
-  },
-  {
-    path: 'tools',
-    component: ToolsComponent,
-  },
-  {
-    path: 'tools/summarization',
-    component: SummarizationComponent,
-  },
-  {
-    path: 'tools/text-to-text',
-    component: Text2textGenerationComponent,
-  },
-  {
-    path: 'tools/translation',
-    component: TranslationComponent,
-  },
-  {
-    path: 'tools/image-to-image',
-    component: ImageToImageComponent,
   },
   {
     path: 'settings',

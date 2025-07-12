35de57c9ac7d7157c03209897d3f6f7967af4354
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-07-12 16:21:38 +0530

    add main lab layout

--- projects/berkelium-labs/src/app/app.routes.ts
+++ projects/berkelium-labs/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Lab } from './lab/lab';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Lab,
+  },
+  {
+    path: '**',
+    component: Lab,
+  },
+];
---
+++ projects/berkelium-labs/src/app/lab/lab.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'berkeliumlabs-lab',
+  imports: [MatCardModule],
+  templateUrl: './lab.html',
+  styleUrl: './lab.scss',
+})
+export class Lab {}

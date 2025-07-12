6a1e06320fdae3df354c3de0a96912baecc1eaa5
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-07-12 18:28:13 +0530

    Styling chatbox

--- projects/berkelium-labs/src/app/lab/lab.ts
+++ projects/berkelium-labs/src/app/lab/lab.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'berkeliumlabs-lab',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './lab.html',
   styleUrl: './lab.scss',
 })
